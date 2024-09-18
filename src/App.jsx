62489c731d636fa7d2d539cf91771f56f5aca3ee
Yusupov Javohir <javohiryyusupov6@gmail.com>
2024-09-18 12:08:12 +0500

    aded news

--- src/App.jsx
+++ src/App.jsx
@@ -165,7 +165,7 @@ import DriverLayout from "./layout/DriverLayout";
 import PageTitle from "./components/PageTitle";
 import HomePageDashboardOwner from "./pages/ownerPages/HomePageDashboardOwner";
 import RegisterPage from "./pages/login/Register";
-import { ThemeProvider } from "./pages/adminPages/themeContext";
+import { ThemeProvider } from "./pages/adminPages/ThemeContex";
 
 export default function App() {
   return (
